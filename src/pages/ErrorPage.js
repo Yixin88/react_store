7123f6012d9ba9d498c05b181b70e74b62cbe7a7
Yixin <yixin88@hotmail.co.uk>
2023-02-14 23:43:38 +0000

    added some info on error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div>
+      <h1>Oopsie! This page does not excist</h1>
+      <NavLink to='/'>Back to home</NavLink>
+    </div>
   )
 }
