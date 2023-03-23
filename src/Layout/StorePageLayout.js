63e4e2de1227193befd40479e56b8400ed391566
Yixin <yixin88@hotmail.co.uk>
2023-03-23 23:06:34 +0000

    renamed Home to ProductMain

--- src/Layout/StorePageLayout.js
+++ src/Layout/StorePageLayout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from '../components/Home'
+import Home from '../components/ProductMain'
 import SideBar from '../components/SideBar'
 
 export default function StorePageLayout() {
