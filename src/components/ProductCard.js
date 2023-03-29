84e16ab948045655620f01fe84433356d7259d25
Yixin <yixin88@hotmail.co.uk>
2023-03-29 16:25:32 +0100

    adding to cart will update cart object array with a new property of quantity

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,7 +6,7 @@ import { CartNum } from '../context/NumInCartContext'
 export default function ProductCard({product}) {
 
 
-  const { addToCart, popUp } = useContext(CartNum)
+  const { addToCart, popUp, addItemToCart } = useContext(CartNum)
 
 
   return (
@@ -19,7 +19,7 @@ export default function ProductCard({product}) {
         <span hidden className="productId">{product.id}</span>
         <span className="rating">{product.rating}/5 ⭐️</span>
         <span className="price">£{product.price}</span>
-        <button className='addToCartBtn' onClick={()=> {addToCart(); popUp()}}>Add to Cart</button>
+        <button className='addToCartBtn' onClick={()=> {addToCart(); popUp(); addItemToCart(product)}}>Add to Cart</button>
     </div>
   )
 }
--- src/context/NumInCartContext.js
+++ src/context/NumInCartContext.js
@@ -7,11 +7,28 @@ export const CartNum = createContext();
 
 export default function NumInCart({children}) {
     const [ cart, setCart ] = useState(0);
+    const [ itemsInCart, setItemsInCart ] = useState([]);
     
     function addToCart() {
         setCart(prevCart => prevCart + 1)
         
     }
+
+    function addItemToCart(item) {
+        if (item.hasOwnProperty('quantity')) {
+            setItemsInCart(itemsInCart.map(cartItem => {
+                if (cartItem.id === item.id) {
+                    cartItem.quantity += 1;
+                }
+                return cartItem;
+            }))
+        } else {
+            item.quantity = 1
+            setItemsInCart([...itemsInCart, item])
+            console.log('2nd run')
+        }
+    }
+    console.log(itemsInCart)
     
     function popUp() {
         const addedToCartNotification = document.querySelector(".addedToCart");
@@ -23,7 +40,7 @@ export default function NumInCart({children}) {
       }
 
     return (
-        <CartNum.Provider value={{cart, addToCart, popUp}}>
+        <CartNum.Provider value={{cart, addToCart, popUp, itemsInCart, addItemToCart}}>
             {children}
         </CartNum.Provider>
     )
