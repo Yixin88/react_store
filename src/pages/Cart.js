5c40c3c03c5dac93c5f959cf3f1ac61561469135
Yixin <yixin88@hotmail.co.uk>
2023-03-31 00:05:55 +0100

    disabled strict mode to prevent rerender twice

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartNum } from '../context/NumInCartContext'
 
 export default function Cart() {
+  const { cart, itemsInCart } = useContext(CartNum);
   return (
-    <main className='subpageMain'>Cart</main>
+    <main className='subpageMain'>
+      <h1>{cart}</h1>
+      {itemsInCart.map(item => {
+        return (<div key={item.id}>
+          <p>{item.title}</p>
+          <p>{item.quantity}</p>
+        </div>)
+      })}
+    </main>
   )
 }
