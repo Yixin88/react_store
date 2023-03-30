9550d50815a169a5247d7c58e4e04c8d572564ed
Yixin <yixin88@hotmail.co.uk>
2023-03-31 00:06:55 +0100

    added some template to cart page and clean up some code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import NumInCart from './context/NumInCartContext';
 import QueryContext from './context/QueryContext';
-import ProductsContextProvider, { ProductsContext } from './context/ProductsContext';
+import ProductsContextProvider from './context/ProductsContext';
 import CategoryContext from './context/CategoryContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
