a3d7209559f9cf91e08cc6f2d9f97f192988d406
Yixin <yixin88@hotmail.co.uk>
2023-04-02 01:56:31 +0100

    added layout for cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,13 +5,28 @@ export default function Cart() {
   const { cart, itemsInCart } = useContext(CartNum);
   return (
     <main className='subpageMain'>
-      <h1>{cart}</h1>
-      {itemsInCart.map(item => {
-        return (<div key={item.id}>
-          <p>{item.title}</p>
-          <p>{item.quantity}</p>
-        </div>)
-      })}
+      <h1>Your Cart</h1>
+      <span/>
+      <div className='cartSection'>
+        {itemsInCart.map(item => {
+          return (
+            <div key={item.id} className='cartCard'>
+              <div className="cartCardImg">
+                <img src={item.images[0]} alt="product" />
+              </div>
+              <div className='cartCartDetail'>
+                <h3 className='cartCartProduct'>{item.title}</h3>
+                <span className='cartPrice'>£{item.price}/Unit</span>
+              </div>
+              <div className="quantityContainer">
+                  <button className="quantityBtn quantityMinus">-</button>
+                  <span className="quantity">{item.quantity}</span>
+                  <button className="quantityBtn quantityPlus">+</button>
+                  <button className="deleteBtn"><img src={require("../assets/trash-bin.png")} alt="delete icon" width="40px" /></button>
+              </div>
+            </div>)
+        })}
+      </div>
     </main>
   )
 }
