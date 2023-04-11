899f6046c043e949aaf7836a3be7a1d4e73cc119
Yixin <yixin88@hotmail.co.uk>
2023-04-11 21:59:39 +0100

    fixed responsiveness for cart page

--- src/components/CartProducts.js
+++ src/components/CartProducts.js
@@ -11,15 +11,17 @@ export default function CartProducts({ item }) {
             <div className="cartCardImg">
                 <img src={item.images[0]} alt="product" />
             </div>
-            <div className='cartCartDetail'>
-                <NavLink className='cartCartProduct' to={`/products/${item.id}`}>{item.title}</NavLink>
-                <span className='cartPrice'>£{item.price}/Unit</span>
-            </div>
-            <div className="quantityContainer">
-                <button className="quantityBtn quantityMinus" onClick={()=>minusFromCart(item)}>-</button>
-                <span className="quantity">{item.quantity}</span>
-                <button className="quantityBtn quantityPlus" onClick={()=>addItemToCart(item)}>+</button>
-                <button className="deleteBtn" onClick={()=>removeFromCart(item)}><img src={require("../assets/trash-bin.png")} alt="delete icon" width="40px" /></button>
+            <div className='cartCard-right'>
+                <div className='cartCartDetail'>
+                    <NavLink className='cartCartProduct' to={`/products/${item.id}`}>{item.title}</NavLink>
+                    <span className='cartPrice'>£{item.price}/Unit</span>
+                </div>
+                <div className="quantityContainer">
+                    <button className="quantityBtn quantityMinus" onClick={()=>minusFromCart(item)}>-</button>
+                    <span className="quantity">{item.quantity}</span>
+                    <button className="quantityBtn quantityPlus" onClick={()=>addItemToCart(item)}>+</button>
+                    <button className="deleteBtn" onClick={()=>removeFromCart(item)}><img src={require("../assets/trash-bin.png")} alt="delete icon" width="40px" /></button>
+                </div>
             </div>
     </div>
     )
